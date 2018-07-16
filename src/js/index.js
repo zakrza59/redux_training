42cc186610533d9c4c6c1772b405a5622ed76eaa
zakrza59 <zakrza59@gmail.com>
2018-07-16 22:03:46 +0200

    Connected to redux

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/index';
+import App from './components/App';
 
 render (
   <Provider store={store}>
